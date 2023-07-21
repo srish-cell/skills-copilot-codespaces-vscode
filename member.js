c473461acb6b05d3eef908e0dcb1d6780e76fe63
Srish N <58882869+srish-cell@users.noreply.github.com>
2023-07-21 16:34:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: {
+            street: '50 Main St.',
+            city: 'Boston',
+            state: 'MA'
+        },
+        skills: ['cooking', 'baking', 'eating']
+    }
+    console.log(member.skills[0]);
+}
